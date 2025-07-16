f9b27ac5aa03f3efe851d703e290c9a29f59dd74
Kirubel <kirubeldesalegn17@gmail.com>
2025-07-16 15:24:56 +0300

    homepage

---
+++ src/pages/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">About Us</h1>
+      <p>about page</p>
+    </div>
+  );
+}
+
+export default About;
